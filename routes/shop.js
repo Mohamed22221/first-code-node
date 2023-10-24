5cf6ae92d7d8cff8535a6069dbcd0a405331c335
Mohamed22221 <hamdymohame7@gmail.com>
2023-10-24 14:33:25 +0300

    done convert html to pug templet

--- routes/shop.js
+++ routes/shop.js
@@ -1,16 +1,20 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const rootDir = require('../util/path');
-const adminData = require('./admin');
+const rootDir = require("../util/path");
+const adminData = require("./admin");
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  console.log('shop.js', adminData.products);
+router.get("/", (req, res, next) => {
+  console.log("shop.js", adminData.products);
   // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-  res.render('shop' , {products : adminData.products , title : "shop"})
+  res.render("shop", {
+    products: adminData.products,
+    title: "shop",
+    path: "shop",
+  });
 });
 
 module.exports = router;
