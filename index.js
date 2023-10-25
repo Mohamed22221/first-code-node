d5a8dde54a8685d665591dc52d565d80a4bb7a3c
Mohamed22221 <hamdymohame7@gmail.com>
2023-10-25 20:21:03 +0300

    conect to mongodb

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
+
+
+
 const express = require("express");
-const bodyParser = require('body-parser')
-const routerCourses = require('./routes/coursesRouter')
-const app = express()
-const port =3000
+const bodyParser = require("body-parser");
+const routerCourses = require("./routes/coursesRouter");
+const app = express();
+const port = 3000;
+
 //data corses
 app.use(bodyParser.json());
-app.use('/api/courses' , routerCourses );
+app.use("/api/courses", routerCourses);
 //listen local server 3000+
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
-
--- routes/coursesRouter.js
+++ routes/coursesRouter.js
@@ -9,7 +9,27 @@ const {
 } = require("../controler/coursesControler");
 
 const { courseValidator } = require("../validator/courses");
+//conect mongo
+const { MongoClient } = require("mongodb");
 
+// Connection URL
+const url =
+  "mongodb+srv://hamdymohame7:MohamedHamdy2003@cluster0.aclhqtt.mongodb.net/?";
+const client = new MongoClient(url);
+
+// Database Name
+
+async function main() {
+  // Use connect method to connect to the server
+  await client.connect();
+  console.log("Connected successfully to server");
+  const db = client.db("learn-node");
+  const collection = db.collection("courses");
+  const data = await collection.find().toArray();
+
+  console.log(data);
+}
+main()
 //get all routes
 router.route("/").get(getAllcourses).post(courseValidator, createCourse);
 router.route("/:id").get(getCourse).patch(updateCourse).delete(deleteCourse);
