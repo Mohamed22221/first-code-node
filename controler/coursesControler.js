6c91a5b496e9023ae942d4dcd26c6b6bfbf03e3c
Mohamed Hamdy <99207823+Mohamed22221@users.noreply.github.com>
2023-10-28 12:47:03 +0200

    handel status JSend specification

--- controler/coursesControler.js
+++ controler/coursesControler.js
@@ -1,10 +1,14 @@
-const { validationResult } = require("express-validator");
-const courseSchema = require("../models/modelCourses");
 const mongoose = require("mongoose");
+const courseSchema = require("../models/modelCourses");
+const { validationResult } = require("express-validator");
+const { SUCCESS, FAIL, ERROR } = require("../utils/httpStatus");
 
 const getAllcourses = async (req, res) => {
   const courses = await courseSchema.find();
-  res.json(courses);
+  res.json({
+    status: SUCCESS,
+    data: { courses },
+  });
 };
 
 const getCourse = async (req, res) => {
@@ -13,25 +17,41 @@ const getCourse = async (req, res) => {
     const course = await courseSchema.findById(dynamicId);
 
     if (!course) {
-      return res.status(404).json({ msg: "not found course" });
+      return res.status(404).json({
+        status: FAIL,
+        data: { course: "Not Found Course" },
+      });
     }
-    res.json(course);
+    res.json({
+      status: SUCCESS,
+      data: { course: course },
+    });
   } catch (err) {
-    return res.status(404).json({ msg: "invalid object id" });
+    return res.status(404).json({
+      status: ERROR,
+      data: null,
+      message: err.message,
+    });
   }
 };
 
 const createCourse = async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(400).json(errors.array());
+    return res.status(400).json({
+      status: FAIL,
+      data: { course: { errors: errors.array() } },
+    });
   }
   const newCourse = await courseSchema({
     _id: new mongoose.Types.ObjectId(),
     ...req.body,
   });
   await newCourse.save();
-  res.status(201).json(newCourse);
+  res.status(201).json({
+    status: SUCCESS,
+    data: newCourse,
+  });
 };
 
 const updateCourse = async (req, res) => {
@@ -41,9 +61,16 @@ const updateCourse = async (req, res) => {
       { _id: id },
       { ...req.body }
     );
-    return res.status(200).json(courseUpdated);
+    return res.status(200).json({
+      status: SUCCESS,
+      data: { course: courseUpdated },
+    });
   } catch (err) {
-    return res.status(404).json({ msg: "not found course" });
+    return res.status(404).json({
+      status: ERROR,
+      data: null,
+      message: err.message,
+    });
   }
 };
 
@@ -51,9 +78,16 @@ const deleteCourse = async (req, res) => {
   const id = req.params.id;
   try {
     const courseDeleted = await courseSchema.deleteOne({ _id: id });
-    return res.status(200).json(courseDeleted);
+    return res.status(200).json({
+      status: SUCCESS,
+      data: { course: courseDeleted },
+    });
   } catch (err) {
-    return res.status(404).json({ msg: "not found course" });
+    return res.status(404).json({
+      status: ERROR,
+      data: null,
+      message: err.message,
+    });
   }
 };
 
